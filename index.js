85bc65a79c2dae18654349249f4322d9772b92ee
yogesh2019 <yogeshkkhichi@gmail.com>
2020-05-02 20:18:30 +0000

    Day1 of Regex

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
+
+// Has 'n' at the end?
 // console.log(/d$/.test("end"));
 // this is the first commit
+
+// includes spaces or not
+// console.log(/\s/.test('hi yoges'));
+// str.inclues(' ');
+// str.indexOf(' ');
+
+/// decode the message
+//console.log("23he323ll232o".match(/\D*/g.join("")));
+
+///// two regular expressions method
+// equals and find are not regex methods
+//RegExp.prototype.exec()
+
+// parseInt() + 1
+///a\w*c/.test(string);
+///a.*c/.test(string)
+
+
+
+////////// replace all the vowels
+
+//console.log("hello".replace(/[aeiou]/g,""));
+
+// and the /i means ignore case 
+
+// return link.split('/')[4];
+// try running without the parenthesis and you would know the //difference
+//console.log("https://www.reddit.com/r/funny/".match(/\/r\/(\w+)/));
+
+// you can also use the -1 
+//console.log(/(r)(\/)(.+)\/$/.exec("https://www.reddit.com/r/funny/"));
+// can also use the slice
+// const REGEXP = /([0-1][0-9]|[2][0-3]):([0-5][0-9])/;
+// const REGEXP = /(?:[0-1][0-9]|[2][0-3]):[0-5][0-9]/;
